94069191d78774c02946411baa619f802b3c5565
Pierce <pierceevans@me.com>
2022-12-22 10:36:11 -0600

    commit tree 14da7259e316c88b5b3eab4e6099fd9b9cf59dd2

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Paul",
+    address: "Country rd",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newObj = {...object};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key, value) {
+    const newObj = {...object};
+    newObj[key] = value;
+    delete newObj.key;
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key, value) {
+    object[key] = value;
+    delete object.key;
+    return object;
+}
